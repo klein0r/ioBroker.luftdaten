ccf6454007b0a197441e120c45412e055d490b2e
Matthias Kleine <matthias@klein0r.de>
2018-05-14 15:24:16 +0200

    Erste Version

--- main.js
+++ main.js
@@ -2,121 +2,94 @@
 /*jslint node: true */
 'use strict';
 
-// you have to require the utils module and call adapter function
-var utils =    require(__dirname + '/lib/utils'); // Get common adapter utils
+var utils = require(__dirname + '/lib/utils'); // Get common adapter utils
+var request = require('request');
 
-// you have to call the adapter function and pass a options object
-// name has to be set and has to be equal to adapters folder name and main file name excluding extension
-// adapter will be restarted automatically every time as the configuration changed, e.g system.adapter.luftdaten.0
 var adapter = new utils.Adapter('luftdaten');
 
-// is called when adapter shuts down - callback has to be called under any circumstances!
-adapter.on('unload', function (callback) {
-    try {
-        adapter.log.info('cleaned everything up...');
-        callback();
-    } catch (e) {
-        callback();
-    }
-});
-
-// is called if a subscribed object changes
-adapter.on('objectChange', function (id, obj) {
-    // Warning, obj can be null if it was deleted
-    adapter.log.info('objectChange ' + id + ' ' + JSON.stringify(obj));
-});
-
-// is called if a subscribed state changes
-adapter.on('stateChange', function (id, state) {
-    // Warning, state can be null if it was deleted
-    adapter.log.info('stateChange ' + id + ' ' + JSON.stringify(state));
-
-    // you can use the ack flag to detect if it is status (true) or command (false)
-    if (state && !state.ack) {
-        adapter.log.info('ack is not set!');
-    }
-});
-
-// Some message was sent to adapter instance over message box. Used by email, pushover, text2speech, ...
-adapter.on('message', function (obj) {
-    if (typeof obj === 'object' && obj.message) {
-        if (obj.command === 'send') {
-            // e.g. send email or pushover or whatever
-            console.log('send command');
-
-            // Send response in callback if required
-            if (obj.callback) adapter.sendTo(obj.from, obj.command, 'Message received', obj.callback);
-        }
-    }
-});
-
-// is called when databases are connected and adapter received configuration.
-// start here!
 adapter.on('ready', function () {
     main();
 });
 
 function main() {
 
-    // The adapters config (in the instance object everything under the attribute "native") is accessible via
-    // adapter.config:
-    adapter.log.info('config test1: '    + adapter.config.test1);
-    adapter.log.info('config test1: '    + adapter.config.test2);
-    adapter.log.info('config mySelect: ' + adapter.config.mySelect);
+    var sensorType = adapter.config.sensorType;
+    var sensorIdentifier = adapter.config.sensorIdentifier;
 
+    adapter.log.info('sensor type: ' + sensorType);
+    adapter.log.info('sensor identifier: ' + sensorIdentifier);
 
-    /**
-     *
-     *      For every state in the system there has to be also an object of type state
-     *
-     *      Here a simple luftdaten for a boolean variable named "testVariable"
-     *
-     *      Because every adapter instance uses its own unique namespace variable names can't collide with other adapters variables
-     *
-     */
+    if (sensorType == "local") {
+        adapter.log.info('local request');
 
-    adapter.setObject('testVariable', {
-        type: 'state',
-        common: {
-            name: 'testVariable',
-            type: 'boolean',
-            role: 'indicator'
-        },
-        native: {}
-    });
+        request(
+            {
+                url: "http://" + sensorIdentifier + "/data.json",
+                json: true
+            },
+            function(error, response, content) {
+                adapter.log.debug('Request done');
 
-    // in this luftdaten all states changes inside the adapters namespace are subscribed
-    adapter.subscribeStates('*');
+                if (!error && response.statusCode == 200) {
 
+                    for (var key in content.sensordatavalues) {
+                        var obj = content.sensordatavalues[key];
 
-    /**
-     *   setState examples
-     *
-     *   you will notice that each setState will cause the stateChange event to fire (because of above subscribeStates cmd)
-     *
-     */
+                        adapter.setObjectNotExists(obj.value_type, {
+                            type: 'state',
+                            common: {
+                                name: obj.value_type,
+                                type: 'number',
+                                role: 'value'
+                            },
+                            native: {}
+                        });
 
-    // the variable testVariable is set to true as command (ack=false)
-    adapter.setState('testVariable', true);
+                        adapter.setState(obj.value_type, {val: obj.value, ack: true});
+                    }
 
-    // same thing, but the value is flagged "ack"
-    // ack should be always set to true if the value is received from or acknowledged from the target system
-    adapter.setState('testVariable', {val: true, ack: true});
+                } else {
+                    adapter.log.error(error);
+                }
+            }
+        );
+    } else if (sensorType == "remote") {
+        adapter.log.info('remote request');
 
-    // same thing, but the state is deleted after 30s (getState will return null afterwards)
-    adapter.setState('testVariable', {val: true, ack: true, expire: 30});
+        request(
+            {
+                url: "http://api.luftdaten.info/v1/sensor/" + sensorIdentifier + "/",
+                json: true
+            },
+            function(error, response, content) {
+                adapter.log.debug('Request done');
 
+                if (!error && response.statusCode == 200) {
 
+                    for (var key in content[0].sensordatavalues) {
+                        var obj = content[0].sensordatavalues[key];
 
-    // examples for the checkPassword/checkGroup functions
-    adapter.checkPassword('admin', 'iobroker', function (res) {
-        console.log('check user admin pw ioboker: ' + res);
-    });
+                        adapter.setObjectNotExists('SDS_' + obj.value_type, {
+                            type: 'state',
+                            common: {
+                                name: 'SDS_' + obj.value_type,
+                                type: 'number',
+                                role: 'value'
+                            },
+                            native: {}
+                        });
 
-    adapter.checkGroup('admin', 'admin', function (res) {
-        console.log('check group user admin group admin: ' + res);
-    });
+                        adapter.setState('SDS_' + obj.value_type, {val: obj.value, ack: true});
+                    }
 
+                } else {
+                    adapter.log.error(error);
+                }
+            }
+        );
+    }
 
-
+    setTimeout(function () {
+        adapter.stop();
+    }, 10000);
 }
