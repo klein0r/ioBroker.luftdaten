368aa86ac0c113dc30523639e30ab84c0a90bf10
Pix <Pix---@users.noreply.github.com>
2018-06-29 21:44:13 +0200

    0.0.4 path might be IP

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ function main() {
     var sensorType = adapter.config.sensorType;
     var sensorIdentifier = adapter.config.sensorIdentifier;
     var sensorName = (adapter.config.sensorName === "") ? sensorIdentifier : adapter.config.sensorName;
-    var path = (sensorType == "local") ? "local." : adapter.config.sensorIdentifier + ".";
+    var path = (sensorType == "local") ? adapter.config.sensorIdentifer.replace("."/g, '_') + "." : adapter.config.sensorIdentifier + ".";
     adapter.log.info('sensor type: ' + sensorType);
     adapter.log.info('sensor identifier: ' + sensorIdentifier);
     adapter.log.info('sensor name: ' + sensorName);
