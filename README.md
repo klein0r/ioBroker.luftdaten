dd0ad2be5dec4dcaa1b012489afba92b45cf8a5f
Matthias Kleine <info@haus-automatisierung.com>
2020-05-14 08:04:36 +0200

    Added missing translations

--- README.md
+++ README.md
@@ -51,6 +51,10 @@ Wait some minutes until the cronjob collects the data for the first time.
 
 ## Changelog
 
+### 0.0.13
+
+* (klein0r) Added missing translations
+
 ### 0.0.12
 
 * (klein0r) Minor bugfixes
--- admin/words.js
+++ admin/words.js
@@ -4,26 +4,74 @@
 systemDictionary = {
     "Sensor Type": {
         "en": "Sensor Type",
-        "de": "Sensor-Typ"
+        "de": "Sensorart",
+        "ru": "Тип датчика",
+        "pt": "Tipo de sensor",
+        "nl": "Sensortype",
+        "fr": "Type de capteur",
+        "it": "Tipo di sensore",
+        "es": "Tipo de sensor",
+        "pl": "Typ czujnika",
+        "zh-cn": "传感器类型"
     },
     "Local": {
         "en": "Local",
-        "de": "Lokal"
+        "de": "Lokal",
+        "ru": "Местный",
+        "pt": "Local",
+        "nl": "Lokaal",
+        "fr": "Local",
+        "it": "Locale",
+        "es": "Local",
+        "pl": "Lokalny",
+        "zh-cn": "本地"
     },
     "Remote": {
         "en": "Remote",
-        "de": "Remote"
+        "de": "Remote",
+        "ru": "Дистанционный пульт",
+        "pt": "Controlo remoto",
+        "nl": "Afgelegen",
+        "fr": "Éloigné",
+        "it": "A distanza",
+        "es": "Remoto",
+        "pl": "Zdalny",
+        "zh-cn": "远程"
     },
     "Sensor Identifier": {
         "en": "Sensor Identifier",
-        "de": "Sensor Kennung"
+        "de": "Sensorkennung",
+        "ru": "Идентификатор датчика",
+        "pt": "Identificador do sensor",
+        "nl": "Sensoridentificatie",
+        "fr": "Identifiant du capteur",
+        "it": "Identificatore del sensore",
+        "es": "Identificador del sensor",
+        "pl": "Identyfikator czujnika",
+        "zh-cn": "传感器标识符"
     },
     "IP (when local) or ID (when remote)": {
         "en": "IP (when local) or ID (when remote)",
-        "de": "IP (wenn lokal) oder ID (wenn Remote)"
+        "de": "IP (wenn lokal) oder ID (wenn remote)",
+        "ru": "IP (когда локально) или ID (когда удаленно)",
+        "pt": "IP (quando local) ou ID (quando remoto)",
+        "nl": "IP (indien lokaal) of ID (indien op afstand)",
+        "fr": "IP (si local) ou ID (lorsque distant)",
+        "it": "IP (quando locale) o ID (quando remoto)",
+        "es": "IP (cuando es local) o ID (cuando es remoto)",
+        "pl": "IP (gdy lokalny) lub ID (gdy zdalny)",
+        "zh-cn": "IP（本地时）或ID（远程时）"
     },
     "Individual name for sensor": {
         "en": "Individual name for sensor",
-        "de": "Eigener Name für diesen Sensor"
+        "de": "Individueller Name für Sensor",
+        "ru": "Индивидуальное имя для датчика",
+        "pt": "Nome individual do sensor",
+        "nl": "Individuele naam voor sensor",
+        "fr": "Nom individuel du capteur",
+        "it": "Nome individuale del sensore",
+        "es": "Nombre individual para sensor",
+        "pl": "Indywidualna nazwa czujnika",
+        "zh-cn": "传感器的个人名称"
     }
 };
