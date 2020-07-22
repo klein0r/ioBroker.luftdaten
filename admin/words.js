6b8f9bdbb1515303dec634f0001ce07e18209496
Matthias Kleine <info@haus-automatisierung.com>
2020-07-22 09:16:03 +0200

    Added link to sensor map - fixes #36

--- admin/words.js
+++ admin/words.js
@@ -38,6 +38,18 @@ systemDictionary = {
         "pl": "Zdalny",
         "zh-cn": "远程"
     },
+    "You can find remote sensors here": {
+        "en": "You can find remote sensors <a target='_blank' href='http://deutschland.maps.luftdaten.info/'>here</a>",
+        "de": "<a target='_blank' href='http://deutschland.maps.luftdaten.info/'>Hier</a> findest Du die Sensoren",
+        "ru": "Вы можете найти удаленные датчики <a target='_blank' href='http://deutschland.maps.luftdaten.info/'>здесь</a>",
+        "pt": "Você pode encontrar sensores remotos <a target='_blank' href='http://deutschland.maps.luftdaten.info/'>aqui</a>",
+        "nl": "Remote sensoren vind je <a target='_blank' href='http://deutschland.maps.luftdaten.info/'>hier</a>",
+        "fr": "Vous pouvez trouver des capteurs à distance <a target='_blank' href='http://deutschland.maps.luftdaten.info/'>ici</a>",
+        "it": "Puoi trovare i sensori remoti <a target='_blank' href='http://deutschland.maps.luftdaten.info/'>qui</a>",
+        "es": "Puedes encontrar sensores remotos <a target='_blank' href='http://deutschland.maps.luftdaten.info/'>aquí</a>",
+        "pl": "Możesz znaleźć zdalne czujniki <a target='_blank' href='http://deutschland.maps.luftdaten.info/'>tutaj</a>",
+        "zh-cn": "您可以<a target='_blank' href='http://deutschland.maps.luftdaten.info/'>在这里</a>找到远程传感器"
+    },
     "Sensor Identifier": {
         "en": "Sensor Identifier",
         "de": "Sensorkennung",
