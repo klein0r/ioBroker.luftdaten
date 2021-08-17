3120e7efaaf835e89fd4b30a530a904256cc7fb7
Matthias Kleine <info@haus-automatisierung.com>
2021-08-17 14:59:13 +0200

    Updated admin interface to maintain multiple sensors in one instance

--- README.md
+++ README.md
@@ -52,6 +52,10 @@ Wait some minutes until the cronjob collects the data for the first time.
 
 ## Changelog
 
+### 2.0.0
+
+* (klein0r) Updated admin interface to maintain multiple sensors in one instance
+
 ### 1.0.3
 
 * (klein0r) Remove non-numeric characters from sensor id
--- main.js
+++ main.js
@@ -22,39 +22,93 @@ class Luftdaten extends utils.Adapter {
     }
 
     async onReady() {
-        const sensorType = this.config.sensorType;
-        const sensorIdentifier = this.config.sensorIdentifier;
-        const sensorName = (this.config.sensorName === '') ? sensorIdentifier : this.config.sensorName;
+        this.getDevices(
+            async (err, states) => {
+
+                const sensorsAll = [];
+                const sensorsKeep = [];
+
+                // Collect all types
+                if (states) {
+                    for (let i = 0; i < states.length; i++) {
+                        const id = this.removeNamespace(states[i]._id);
+
+                        // Check if the state is a direct child (device)
+                        if (id.indexOf('.') === -1) {
+                            sensorsAll.push(id);
+                            this.log.debug('sensor device exists: ' + id);
+                        }
+                    }
+                }
+
+                const sensors = this.config.sensors;
+
+                if (sensors && Array.isArray(sensors)) {
+                    this.log.debug('Found ' + sensors.length + ' sensors, fetching data');
+
+                    for (const s in sensors) {
+                        const sensor = sensors[s];
+
+                        const sensorName = await this.getSensorData(sensor);
+
+                        sensorsKeep.push(sensorName);
+                        this.log.debug('sensor found: ' + sensorName);
+                    }
+                } else {
+                    this.log.warn('No sensors configured');
+                }
+
+                // Delete non existent sensors
+                for (let i = 0; i < sensorsAll.length; i++) {
+                    const id = sensorsAll[i];
+
+                    if (sensorsKeep.indexOf(id) === -1) {
+                        this.delObject(id, {recursive: true}, () => {
+                            this.log.debug('sensor device deleted: ' + id);
+                        });
+                    }
+                }
+
+                this.killTimeout = setTimeout(this.stop.bind(this), 15 * 1000); // 15 Seconds
+            }
+        );
+    }
+
+    async getSensorData(sensor) {
+
+        const unitList = {
+            P1: 'µg/m³',
+            P2: 'µg/m³',
+            temperature: '°C',
+            humidity: '%',
+            pressure: 'Pa',
+            pressure_at_sealevel: 'Pa',
+            noise: 'dB(A)',
+            signal: 'dB(A)',
+            min_micro: 'µs',
+            max_micro: 'µs'
+        };
+
+        const roleList = {
+            P1: 'value.ppm',
+            P2: 'value.ppm',
+            temperature: 'value.temperature',
+            humidity: 'value.humidity',
+            pressure: 'value.pressure',
+            pressure_at_sealevel: 'value.pressure',
+            noise: 'value',
+            signal: 'value',
+            min_micro: 'value',
+            max_micro: 'value'
+        };
+
+        const sensorType = sensor.type;
+        const sensorIdentifier = sensor.identifier;
+        const sensorName = (sensor.name === '') ? sensorIdentifier : sensor.name;
 
         if (sensorIdentifier && sensorName) {
             const deviceName = (sensorType == 'local') ? sensorIdentifier.replace(/\./g, '_') : sensorIdentifier.replace(/\D/g,'');
             const path = deviceName + '.';
-
-            const unitList = {
-                P1: 'µg/m³',
-                P2: 'µg/m³',
-                temperature: '°C',
-                humidity: '%',
-                pressure: 'Pa',
-                pressure_at_sealevel: 'Pa',
-                noise: 'dB(A)',
-                signal: 'dB(A)',
-                min_micro: 'µs',
-                max_micro: 'µs'
-            };
-
-            const roleList = {
-                P1: 'value.ppm',
-                P2: 'value.ppm',
-                temperature: 'value.temperature',
-                humidity: 'value.humidity',
-                pressure: 'value.pressure',
-                pressure_at_sealevel: 'value.pressure',
-                noise: 'value',
-                signal: 'value',
-                min_micro: 'value',
-                max_micro: 'value'
-            };
 
             this.log.debug('sensor type: ' + sensorType + ', sensor identifier: ' + sensorIdentifier + ', sensor name: ' + sensorName);
 
@@ -77,6 +131,18 @@ class Luftdaten extends utils.Adapter {
             });
             this.setState(path + 'name', {val: sensorName, ack: true});
 
+            await this.setObjectNotExistsAsync(path + 'responseCode', {
+                type: 'state',
+                common: {
+                    name: 'responseCode',
+                    type: 'number',
+                    role: 'value',
+                    read: true,
+                    write: false
+                },
+                native: {}
+            });
+
             if (sensorType == 'local') {
                 this.log.debug('local request started');
 
@@ -93,17 +159,6 @@ class Luftdaten extends utils.Adapter {
                         this.log.debug('local request done');
                         this.log.debug('received data (' + response.status + '): ' + JSON.stringify(content));
 
-                        await this.setObjectNotExistsAsync(path + 'responseCode', {
-                            type: 'state',
-                            common: {
-                                name: 'responseCode',
-                                type: 'number',
-                                role: 'value',
-                                read: true,
-                                write: false
-                            },
-                            native: {}
-                        });
                         this.setState(path + 'responseCode', {val: response.status, ack: true});
 
                         if (content && Object.prototype.hasOwnProperty.call(content, 'sensordatavalues')) {
@@ -186,17 +241,6 @@ class Luftdaten extends utils.Adapter {
                         this.log.debug('remote request done');
                         this.log.debug('received data (' + response.status + '): ' + JSON.stringify(content));
 
-                        await this.setObjectNotExistsAsync(path + 'responseCode', {
-                            type: 'state',
-                            common: {
-                                name: 'responseCode',
-                                type: 'number',
-                                role: 'value',
-                                read: true,
-                                write: false
-                            },
-                            native: {}
-                        });
                         this.setState(path + 'responseCode', {val: response.status, ack: true});
 
                         if (content && Array.isArray(content) && content.length > 0) {
@@ -257,7 +301,7 @@ class Luftdaten extends utils.Adapter {
                                     },
                                     native: {}
                                 });
-                                this.setState(path + 'location.longitude', {val: sensorData.location.longitude, ack: true});
+                                this.setState(path + 'location.longitude', {val: parseFloat(sensorData.location.longitude), ack: true});
 
                                 await this.setObjectNotExistsAsync(path + 'location.latitude', {
                                     type: 'state',
@@ -271,7 +315,7 @@ class Luftdaten extends utils.Adapter {
                                     },
                                     native: {}
                                 });
-                                this.setState(path + 'location.latitude', {val: sensorData.location.latitude, ack: true});
+                                this.setState(path + 'location.latitude', {val: parseFloat(sensorData.location.latitude), ack: true});
 
                                 await this.setObjectNotExistsAsync(path + 'location.altitude', {
                                     type: 'state',
@@ -285,7 +329,7 @@ class Luftdaten extends utils.Adapter {
                                     },
                                     native: {}
                                 });
-                                this.setState(path + 'location.altitude', {val: sensorData.location.altitude, ack: true});
+                                this.setState(path + 'location.altitude', {val: parseFloat(sensorData.location.altitude), ack: true});
 
                                 await this.setObjectNotExistsAsync(path + 'timestamp', {
                                     type: 'state',
@@ -310,23 +354,34 @@ class Luftdaten extends utils.Adapter {
                             // The request was made and the server responded with a status code
 
                             this.log.warn('received error ' + error.response.status + ' response from remote sensor ' + sensorIdentifier + ' with content: ' + JSON.stringify(error.response.data));
+                            this.setState(path + 'responseCode', {val: error.response.status, ack: true});
+
                         } else if (error.request) {
                             // The request was made but no response was received
                             // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
                             // http.ClientRequest in node.js
                             this.log.error(error.message);
+                            this.setState(path + 'responseCode', {val: error.response.status, ack: true});
                         } else {
                             // Something happened in setting up the request that triggered an Error
                             this.log.error(error.message);
+                            this.setState(path + 'responseCode', {val: -1, ack: true});
                         }
                     }
                 );
             }
+
+            return deviceName;
         } else {
             this.log.debug('sensor type and/or sensor identifier not defined');
-        }
 
-        this.killTimeout = setTimeout(this.stop.bind(this), 15000);
+            return null;
+        }
+    }
+
+    removeNamespace(id) {
+        const re = new RegExp(this.namespace + '*\.', 'g');
+        return id.replace(re, '');
     }
 
     onUnload(callback) {
